70e0ff83bb0f10270b44dbd63eca871f59cffe99
Mems2kur <tukurmaimuna15@gmail.com>
2023-03-23 15:06:24 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_isupper - print upper case letters
+ *
+ *@c: ASCII CODE
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
