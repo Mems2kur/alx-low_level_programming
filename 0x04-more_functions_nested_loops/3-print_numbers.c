84abb94438eaedfe1b458173faab68fd4d4072ce
Mems2kur <tukurmaimuna15@gmail.com>
2023-03-23 16:04:51 -0700

    printnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_numbers- print upper case letters
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
