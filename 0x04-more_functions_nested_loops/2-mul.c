53fd32f80640d4643b66e114a2630197f053d4bd
Mems2kur <tukurmaimuna15@gmail.com>
2023-03-23 15:37:37 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *mul - print upper case letters
+ *
+ *@a: first int
+ *@b: second int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
